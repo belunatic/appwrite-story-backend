23916ddc77033e88fc9280126654066f12ec5331
belunatic <abelluvyou@yahoo.com>
2025-02-26 12:04:55 -0800

    Add morgan for logging and update server port configuration

--- server/app.js
+++ server/app.js
@@ -2,14 +2,17 @@ const express = require("express");
 const cors = require("cors");
 const sdk = require("node-appwrite");
 const storiesRoutes = require("./routes/stories");
+const morgan = require("morgan");
 
 const app = express();
 
 app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
 require("dotenv").config();
 
 app.use("/", storiesRoutes);
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 5000, () => {
 	console.log("Server is running on port 3000");
 });
