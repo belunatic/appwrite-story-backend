e03d0d8c7f41681f7a99c188aa72cf242a6dcd15
belunatic <abelluvyou@yahoo.com>
2025-02-28 11:06:46 -0800

    Add StoryForm component and refactor AddStory and EditStory pages to use it

---
+++ Client/src/components/StoryForm.jsx
@@ -0,0 +1,42 @@
+import PropTypes from "prop-types";
+
+const StoryForm = (props) => {
+	return (
+		<div>
+			<h1>{props.formTitle}</h1>
+			<p>{props.error}</p>
+			<form onSubmit={props.handleSubmit}>
+				<label htmlFor="title">Title</label>
+				<input
+					type="text"
+					name="title"
+					id="title"
+					value={props.title}
+					onChange={(e) => props.setTitle(e.target.value)}
+				/>
+				<label htmlFor="body">Story</label>
+				<textarea
+					name="body"
+					id="body"
+					value={props.body}
+					placeholder="So what's up?"
+					onChange={(e) => props.setBody(e.target.value)}></textarea>
+				<button>Submit</button>
+			</form>
+		</div>
+	);
+};
+
+//props validation
+StoryForm.propTypes = {
+	title: PropTypes.string,
+	body: PropTypes.string,
+	formTitle: PropTypes.string,
+	setTitle: PropTypes.func,
+	setBody: PropTypes.func,
+	handlesSubmit: PropTypes.func,
+	error: PropTypes.string,
+	setError: PropTypes.func,
+};
+
+export default StoryForm;
